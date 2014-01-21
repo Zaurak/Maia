9ee9ef2e1a3e5ea68d226ee47728b0cdde73529a
Gaetan Fleury <fleury.gaetan@gmail.com>
2014-01-21 16:40:17 +0100

    Allow user to sign in and sign out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
 	before_save { self.email = email.downcase }
+	before_create :create_remember_token
 
 	validates :name, 
 						presence: true, 
@@ -13,4 +14,19 @@ class User < ActiveRecord::Base
 						
 	has_secure_password
 	validates :password, length: { minimum: 6 }
+
+def User.new_remember_token
+	SecureRandom.urlsafe_base64
+end
+
+def User.encrypt(token)
+	Digest::SHA1.hexdigest(token.to_s)
+end
+
+private
+	
+	def create_remember_token
+		self.remember_token = User.encrypt(User.new_remember_token)
+	end
+
 end
