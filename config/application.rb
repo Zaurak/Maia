09fb3d23a6a5fb889cb1f57511c203412d2b600b
Gaetan Fleury <fleury.gaetan@gmail.com>
2014-01-21 12:24:19 +0100

    Add a basic user model (name, email, secure password)

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,7 @@ module Maia
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
+    I18n.enforce_available_locales = true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Maia::Application.routes.draw do
+  get "users/new"
   
   root 'static_pages#home'
+  match '/signup', to: 'users#new', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
