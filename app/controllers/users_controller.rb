15eb39d8a8b54bef7e6e47cf221712d75a60cf8b
Gaetan Fleury <fleury.gaetan@gmail.com>
2014-01-22 13:19:02 +0100

    Add user edit, update and index actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,17 @@
 class UsersController < ApplicationController
-  
+  before_action :signed_in_user,  only: [:index, :edit, :update]
+  before_action :correct_user,    only: [:edit, :update]
+
   def show
   	@user = User.find(params[:id])
   end
 
   def new
   	@user = User.new
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
   end
 
   def create
@@ -19,6 +25,17 @@ class UsersController < ApplicationController
   	end
   end
 
+  def edit 
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 
   private
 
@@ -26,4 +43,18 @@ class UsersController < ApplicationController
   		params.require(:user).permit( :name, :email, :password,
   																	:password_confirmation )
   	end
+
+    # Before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
--- spec/requests/user_page_spec.rb
+++ spec/requests/user_page_spec.rb
@@ -4,6 +4,31 @@ describe "User pages" do
 
   subject { page }
 
+  describe "index" do
+    let(:user) { FactoryGirl.create(:user) }
+    before(:each) do
+      sign_in user
+      visit users_path
+    end
+
+    it { should have_title('All users') }
+    it { should have_content('All users') }
+
+    describe "pagination" do
+
+      before(:all) { 30.times { FactoryGirl.create(:user) } }
+      after(:all)  { User.delete_all }
+
+      it { should have_selector('div.pagination') }
+
+      it "should list each user" do
+        User.paginate(page: 1).each do |user|
+          expect(page).to have_selector('li', text: user.name)
+        end
+      end
+    end
+  end
+
   describe "signup page" do
     before { visit signup_path }
 
@@ -22,7 +47,7 @@ describe "User pages" do
     		fill_in "Name",							with: "Example User"
     		fill_in "Email", 						with: "user@example.com"
     		fill_in "Password", 				with: "foobar"
-    		fill_in "Confirmation", 		with: "foobar"
+    		fill_in "Confirm Password", 		with: "foobar"
     	end
 
     	it "should create a user" do
@@ -46,5 +71,43 @@ describe "User pages" do
 
   	it { should have_content(user.name) }
   	it { should have_title(user.name) }
+  end
+
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
+
+    describe "page" do
+      it { should have_content("Update your profile") }
+      it { should have_title("Edit user") }
+      it { should have_link('Change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
+    end
+
+    describe "with valid information" do
+      let(:new_name)  { "New Name" }
+      let(:new_email) { "new@example.com" }
+      before do
+        fill_in "Name",             with: new_name
+        fill_in "Email",            with: new_email
+        fill_in "Password",         with: user.password
+        fill_in "Confirm Password", with: user.password
+        click_button "Save changes"
+      end
+
+      it { should have_title(new_name) }
+      it { should have_selector('div.alert.alert-success') }
+      it { should have_link('Sign out', href: signout_path) }
+      specify { expect(user.reload.name).to  eq new_name }
+      specify { expect(user.reload.email).to eq new_email }
+    end
   end
 end
