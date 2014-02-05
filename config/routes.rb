63f03c499f502ed9d84e040609ed40a6c73c0e3c
asma <asma.alibert@gmail.com>
2014-02-05 01:33:20 +0100

    Modification of Home and About pages

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Maia::Application.routes.draw do
   
   root 'static_pages#home'
   match '/help',                    to: 'static_pages#help',     via: 'get'
+  match '/about',                  to: 'static_pages#about',     via: 'get'
   match '/signup',                  to: 'users#new',             via: 'get'
   match '/signin',                  to: 'sessions#new',          via: 'get'
   match '/signout',                 to: 'sessions#destroy',      via: 'delete'
